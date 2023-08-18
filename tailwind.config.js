7cd3caa4266284d7331c613434870fc82a569094
David Akang <akangdavid68@yahoo.com>
2023-08-18 09:12:36 +0100

    Enabled Dosis Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#ffbc00",
+        light: "#f4f4f4",
+        medium: "#ccc",
+        dark: "#333",
+      },
+      backgroundImage: {
+        showcase: "url('../project_resources/img/showcase.jpg')", //relative to output.css file
+      },
+      maxWidth: {
+        website: "1280px",
+      },
+      fontFamily: {
+        dosis: ["Dosis", "sans-serif", "Arial"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
