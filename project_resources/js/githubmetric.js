9ada1770860bfb82018522d27ab8360a447d118c
David Akang <akangdavid68@yahoo.com>
2023-08-23 10:19:07 +0100

    Process Section & Footer Complete

--- project_resources/js/githubmetric.js
+++ project_resources/js/githubmetric.js
@@ -1,25 +1,8 @@
-// githubmetric.js
-function fetchRepoCount() {
-  // TODO: Have the token be replaced by a environvironment variable when deployed to netlify
-  const accessToken = "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"; // Replace with your GitHub access token
-  const username = "DBAKANG-GIT"; // Replace with your GitHub username
-
-  const apiUrl = `https://api.github.com/users/${username}`;
-
-  fetch(apiUrl, {
-    headers: {
-      Authorization: `token ${accessToken}`,
-    },
+fetch("https://api.github.com/users/DBAKANG-GIT") //Api to hit
+  .then((result) => result.json()) //configure the promise which is returned and converted to json
+  .then((data) => {
+    document.getElementById("repoCount").textContent = data.public_repos; //Set the response to this element//
   })
-    .then((response) => response.json())
-    .then((data) => {
-      const repoCount = data.public_repos;
-      document.getElementById(
-        "repoCount"
-      ).textContent = `GitHub Repository Count: ${repoCount}`;
-    })
-    .catch((error) => {
-      console.error("Error fetching data from GitHub API", error);
-    });
-}
-fetchRepoCount();
+  .catch((error) => {
+    console.error("Error fetching data from GitHub API", error);
+  });
